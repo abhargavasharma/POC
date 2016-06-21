95be5dee1f5a21452e84173a4f9af249ad3bbc1e
Bhargavasharma Appala <BhargavasharmaA@bluedoor.local>
2016-06-21 18:39:13 +1000

    Ext method sample

---
+++ ExtensionMethodsSample/ExtensionMethodsSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var adv = new Adviser { ID = "1" , Name = "bhargav" };
+            adv.GetBalance();
+            Console.ReadKey();
+        }
+    }
+
+    class Adviser
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    static class ExtensionMethods
+    {
+        public static void GetBalance(this Adviser adv)
+        {
+
+            Console.WriteLine("Logging in Ext method{0}",adv.Name);
+        }
+    }
+}
