326f58bd189aafb3d46e70b612179988a7fee6d1
DESKTOP-DC02588\Bhargav <bhargavasharma.a@gmail.com>
2016-12-15 21:24:02 +1100

    Dapper POC

---
+++ DapperPOC/DBScripts/dbo.Product.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NULL, 
+    [ParentId] NVARCHAR(50) NULL, 
+    [Description] NVARCHAR(250) NULL, 
+    [URL] NVARCHAR(250) NULL
+)
